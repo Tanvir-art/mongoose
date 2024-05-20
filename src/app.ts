6747b515481c97a2f23440dd921d5d949bb2a492
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2024-05-20 11:33:04 +0600

    mongoose projectOne

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
+
+// console.log(process.cwd())
+
+// C:\Users\DELL\Desktop\mongoose\firstProject>
